e4cb1ae699835bbbfbbf97c75a5dd935b3ef689b
Edrynaath <47075488+wushu233@users.noreply.github.com>
2024-10-28 22:40:46 +1100

    Update run.py

--- run.py
+++ run.py
@@ -2,56 +2,46 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-from tensorflow.keras.losses import SparseCategoricalCrossentropy  # Import this explicitly
-
-
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.inception_v3 import preprocess_input as inception_preprocess_input
+import os
 @st.cache(allow_output_mutation=True)
 def load_model():
-	model = tf.keras.models.load_model(
-        'bush.hdf5',
-        custom_objects={'SparseCategoricalCrossentropy': SparseCategoricalCrossentropy()}
-    )
-	return model
+    model = tf.keras.models.load_model("best_trained_save.h5")
 
+    return model
 
-def predict_class(image, model):
+def predict_class(img, model):
+    # Resize the image
+    img = img.resize((224, 224))  # Resize to 224x224
+    processed_img = image.img_to_array(img)  # Convert the image to array
+    processed_img = np.expand_dims(processed_img, axis=0)  # Add batch dimension
+    processed_img = inception_preprocess_input(processed_img)  # Preprocess image
 
-	image = tf.cast(image, tf.float32)
-	image = tf.image.resize(image, [180, 180])
-
-	image = np.expand_dims(image, axis = 0)
-
-	prediction = model.predict(image)
-
-	return prediction
-
+    # Get prediction using the network
+    predictions = model.predict(processed_img)[0]
+    return predictions
 
 model = load_model()
 st.title('BushFire Classifier')
 
 file = st.file_uploader("Upload an image", type=["jpg", "png"])
 
-
 if file is None:
-	st.text('Waiting for upload....')
-
+    st.text('Waiting for upload....')
 else:
-	slot = st.empty()
-	slot.text('Running inference....')
+    slot = st.empty()
+    slot.text('Running inference....')
 
-	test_image = Image.open(file)
+    test_image = Image.open(file)
+    st.image(test_image, caption="Input Image", width=400)
 
-	st.image(test_image, caption="Input Image", width = 400)
+    pred = predict_class(test_image, model)
 
-	pred = predict_class(np.asarray(test_image), model)
+    classes = ['fire', 'no_fire', 'start_fire']
+    result = classes[np.argmax(pred)]
 
-	class_names = ['fire', 'nofire']
-
-	result = class_names[np.argmax(pred)]
-
-	output = 'The image is a ' + result
-
-	slot.text('Done')
-
-	st.success(output)
-
+    output = 'The image is a ' + result
+    slot.text('Done')
+    st.success(output)
