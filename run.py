b15b4599e5a2d4e4f00c8a72953bcfda8d4a13ce
Edrynaath <47075488+wushu233@users.noreply.github.com>
2024-10-28 19:25:55 +1100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.inception_v3 import preprocess_input as inception_preprocess_input
+import os
+@st.cache(allow_output_mutation=True)
+def load_model():
+    base_dir = os.path.dirname(__file__)
+    model_path = os.path.join(base_dir, "model-saves", "Inception_based", "best_trained_save.h5")
+
+    model = tf.keras.models.load_model(model_path)
+
+    return model
+
+def predict_class(img, model):
+    # Resize the image
+    img = img.resize((224, 224))  # Resize to 224x224
+    processed_img = image.img_to_array(img)  # Convert the image to array
+    processed_img = np.expand_dims(processed_img, axis=0)  # Add batch dimension
+    processed_img = inception_preprocess_input(processed_img)  # Preprocess image
+
+    # Get prediction using the network
+    predictions = model.predict(processed_img)[0]
+    return predictions
+
+model = load_model()
+st.title('BushFire Classifier')
+
+file = st.file_uploader("Upload an image", type=["jpg", "png"])
+
+if file is None:
+    st.text('Waiting for upload....')
+else:
+    slot = st.empty()
+    slot.text('Running inference....')
+
+    test_image = Image.open(file)
+    st.image(test_image, caption="Input Image", width=400)
+
+    pred = predict_class(test_image, model)
+
+    classes = ['fire', 'no_fire', 'start_fire']
+    result = classes[np.argmax(pred)]
+
+    output = 'The image is a ' + result
+    slot.text('Done')
+    st.success(output)
